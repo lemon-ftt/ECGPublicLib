511182bfd9812f2d57ef761f8632591ed92c7572
Fighter-FT <tan@yanyandeMac-mini.local>
2017-03-08 16:42:58 +0800

    ECGLayoutConstraint
    ECGLayoutConstraintECGLayoutConstraint

--- ECGPublicLibDemo/ECGPublicLibDemo/ECGCustomCell/ECGCustomCell.m
+++ ECGPublicLibDemo/ECGPublicLibDemo/ECGCustomCell/ECGCustomCell.m
@@ -9,7 +9,7 @@
 #import "ECGCustomCell.h"
 #import "Masonry.h"
 #import "UIImageView+WebCache.h"
-
+#import "ECGLayoutConstraint.h"
 
 
 @implementation ECGCustomCell
@@ -62,32 +62,42 @@
      *可以有部分控件高度不固定，
      *但是必须有一个控件撑起来cell，
      *比如userPhoto控件从顶部出发，自我介绍控件高度没写，顶部与头像控件底部关联，底部等于cell的底部
-    */
-    
+     */
+    __weak typeof(self) weakSelf = self;
     //头像
     [_userPhoto mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.left.mas_equalTo(10);
-        make.top.mas_equalTo(10);
-        make.width.height.mas_equalTo(60);
+        make.left.mas_equalTo([weakSelf handleLayout:20]);
+        make.top.mas_equalTo([weakSelf handleLayout:20]);
+        make.width.height.mas_equalTo([weakSelf handleLayout:120]);
     }];
     //用户名
     [_nameLabel mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.left.equalTo(_userPhoto.mas_right).offset(5);
-        make.width.offset(100);
+        make.left.equalTo(_userPhoto.mas_right).offset([weakSelf handleLayout:10]);
+        make.width.offset([weakSelf handleLayout:200]);
         make.centerY.equalTo(_userPhoto.mas_centerY);
-        make.height.mas_equalTo(20);
+        make.height.mas_equalTo([weakSelf handleLayout:40]);
     }];
     //自我介绍
     [_introductionLabel mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.left.mas_equalTo(10);
-        make.right.equalTo(_introductionLabel.superview.mas_right).offset(-10);
-        make.top.equalTo(_userPhoto.mas_bottom).offset(10);
-        make.bottom.equalTo(_introductionLabel.superview.mas_bottom).offset(-5);
+        make.left.mas_equalTo([weakSelf handleLayout:20]);
+        make.right.equalTo(_introductionLabel.superview.mas_right).offset(-[weakSelf handleLayout:20]);
+        make.top.equalTo(_userPhoto.mas_bottom).offset([weakSelf handleLayout:20]);
+        make.bottom.equalTo(_introductionLabel.superview.mas_bottom).offset(-[weakSelf handleLayout:10]);
     }];
     
     self.nameLabel.text = _ECGModel.name;
     self.introductionLabel.text = _ECGModel.introduction;
     [self.userPhoto sd_setImageWithURL:[NSURL URLWithString:_ECGModel.imgName] placeholderImage:[UIImage imageNamed:@"defaultHead"]];
+}
+
+/**
+ 处理多屏幕显示
+ 
+ @param value <#value description#>
+ @return <#return value description#>
+ */
+- (CGFloat)handleLayout:(CGFloat)value {
+    return [ECGLayoutConstraint getConstrainlWithValueFrom6:value];
 }
 
 @end
